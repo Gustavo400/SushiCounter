f41ed7f88f6a60b6f2cacc3a29f35e0d5dd1609f
Gustavo Rios <43116946+Gustavo400@users.noreply.github.com>
2024-07-09 11:13:02 -0400

    added styles file and deleted old icons

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import styles from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Home Screen!</Text>
+      <Text style={styles.text}>Home Screen!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
