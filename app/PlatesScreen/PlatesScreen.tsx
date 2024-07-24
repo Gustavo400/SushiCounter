3897cc53b13107c178f8ca7e324d410a5b9a2bcd
Gustavo Rios <43116946+Gustavo400@users.noreply.github.com>
2024-07-24 15:55:12 -0400

    added sushi counter tracking and fixed render updating

--- app/PlatesScreen/PlatesScreen.tsx
+++ app/PlatesScreen/PlatesScreen.tsx
@@ -3,19 +3,38 @@ import styles from "./PlatesScreen.styles";
 import { PlatesScreenProps } from "../../navigation/nav-types";
 import Colors from "../design";
 import SushiPlate from "../../assets/sushi-plate";
+import { useState } from "react";
 
-export default function PlatesScreen({ route, navigation }: PlatesScreenProps) {
+type PlayerData = {
+  id: number;
+  sushiCount: number;
+};
+
+export default function PlatesScreen({ route }: PlatesScreenProps) {
   const playerCount = route.params.numberOfPlayers;
+  const initialGameData: PlayerData[] = Array.from(
+    { length: playerCount },
+    (_, index) => ({
+      id: index + 1,
+      sushiCount: 0,
+    })
+  );
+  const [gameData, setGameData] = useState<PlayerData[]>(initialGameData);
+
+  // ordered array of colors for the plate backgrounds
   const colorsArr = Object.values(Colors);
-  const data = Array.from({ length: playerCount }, (_, index) => ({
-    key: `Player ${index + 1}`,
-  }));
+
+  const incrementSushi = (index: number) => {
+    let updatedGameData = gameData;
+    updatedGameData[index].sushiCount++;
+    setGameData([...updatedGameData]);
+  };
 
   return (
     <FlatList
       style={styles.container}
       numColumns={playerCount > 4 ? 2 : 1}
-      data={data}
+      data={gameData}
       scrollEnabled={false}
       contentContainerStyle={{ display: "flex", flex: 1 }}
       renderItem={({ index, item }) => (
@@ -24,10 +43,15 @@ export default function PlatesScreen({ route, navigation }: PlatesScreenProps) {
             styles.plateBox,
             { backgroundColor: colorsArr[index], flex: 1 },
           ]}
+          key={index}
         >
-          <TouchableOpacity style={styles.plateTouchable}>
+          <TouchableOpacity
+            style={styles.plateTouchable}
+            onPress={() => incrementSushi(index)}
+          >
             <View style={styles.textBox}>
-              <Text style={styles.displayText}>{item.key}</Text>
+              <Text style={styles.displayText}>Player {item.id}</Text>
+              <Text style={styles.displayText}>{item.sushiCount}</Text>
               <View style={styles.plate}>
                 <SushiPlate />
               </View>
